c39a89b2ea3d0c144572d814d119d95ce6936c11
beckynayere <rebecca.nayere@gmail.com>
2023-07-28 01:37:52 +0300

    add main.js changes

--- src/vue.config.js
+++ src/vue.config.js
@@ -7,26 +7,9 @@
 
 // vue.config.js
 
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  base: '/Student/', // Use "base" instead of "publicPath"
-  build: {
-    sourcemap: true, // Use "sourcemap" instead of "productionSourceMap"
-  },
-});
-
-// import { fileURLToPath, URL } from 'node:url'
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
+// import { fileURLToPath, URL } from 'node:url';
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
 
 // export default defineConfig({
 //   plugins: [vue()],
@@ -35,11 +18,28 @@ export default defineConfig({
 //       '@': fileURLToPath(new URL('./src', import.meta.url)),
 //     },
 //   },
-//     module.exports = {
-//         publicPath: process.env.NODE_ENV === 'production' ? '/Student/' : '/',
-//     },
-  
-//   // Other configurations...
-//   productionSourceMap: true,
+//   base: '/Student/', // Use "base" instead of "publicPath"
+//   build: {
+//     sourcemap: true, // Use "sourcemap" instead of "productionSourceMap"
+//   },
 // });
+
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+    module.exports = {
+        publicPath: process.env.NODE_ENV === 'production' ? '/Student/' : '/',
+    },
+  
+  // Other configurations...
+  productionSourceMap: true,
+});
 
