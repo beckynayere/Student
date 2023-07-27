524455e1b68de3e89204637fd6e5953cd6775849
beckynayere <rebecca.nayere@gmail.com>
2023-07-28 01:29:24 +0300

    vite configuration

--- src/vue.config.js
+++ src/vue.config.js
@@ -6,9 +6,10 @@
   
 
 // vue.config.js
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
@@ -17,11 +18,28 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-    module.exports = {
-        publicPath: process.env.NODE_ENV === 'production' ? '/Student/' : '/',
-    },
-  
-  // Other configurations...
-  productionSourceMap: true,
+  base: '/Student/', // Use "base" instead of "publicPath"
+  build: {
+    sourcemap: true, // Use "sourcemap" instead of "productionSourceMap"
+  },
 });
+
+// import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//     },
+//   },
+//     module.exports = {
+//         publicPath: process.env.NODE_ENV === 'production' ? '/Student/' : '/',
+//     },
+  
+//   // Other configurations...
+//   productionSourceMap: true,
+// });
 
